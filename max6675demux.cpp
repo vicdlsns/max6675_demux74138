75e0e8adeb6cfaa92a3e65909bc3cb138606566e
vdl149 <30540488+vicdlsns@users.noreply.github.com>
2020-08-07 16:48:45 -0300

    Add files via upload

---
+++ max6675demux.cpp
@@ -0,0 +1,102 @@
+// this library is public domain. enjoy!
+// www.ladyada.net/learn/sensors/thermocouple
+
+#ifdef __AVR
+  #include <avr/pgmspace.h>
+#elif defined(ESP8266)
+  #include <pgmspace.h>
+#endif
+#include <util/delay.h>
+#include <stdlib.h>
+#include "max6675demux.h"
+
+MAX6675demux::MAX6675demux(int8_t SCLK, int8_t* DEMUX_PINS, int8_t EN, int8_t MISO, uint8_t ACTIVE_EN=LOW) {
+  sclk = SCLK;
+  en = EN;
+  miso = MISO;
+  demux_pins = DEMUX_PINS; //shallow copy
+  active_en = ACTIVE_EN;
+  inactive_en = !ACTIVE_EN;
+
+  //define pin modes
+  pinMode(en, OUTPUT);
+  pinMode(sclk, OUTPUT); 
+  pinMode(miso, INPUT);
+  
+  int len=sizeof(demux_pins)/sizeof(int8_t);
+  for (int i=0;i<len;i++)
+  {
+	  pin=demux_pins[i];
+	  pinMode(pin, OUTPUT); //set all pins to output
+	  digitalWrite(pin, LOW); //set all pins to low
+  }
+
+  digitalWrite(en, inactive_en);
+}
+
+void MAX6675demux::select(int8_t index)
+{
+	int8_t num = index;
+	int8_t i = 0;
+	
+	while (num>0)
+	{
+		int8_t remainder=num%2;
+		
+		int8_t pin = demux_pins[i];
+		digitalWrite(pin, remainder);
+		
+		num/=2;
+		i++;
+	}
+}
+
+double MAX6675demux::readCelsius(int8_t index) {
+
+  uint16_t v;
+
+  select(index);
+  
+  digitalWrite(en, active_en);
+  _delay_ms(1);
+
+  v = spiread();
+  v <<= 8;
+  v |= spiread();
+
+  digitalWrite(en, inactive_en);
+
+  if (v & 0x4) {
+    // uh oh, no thermocouple attached!
+    return NAN; 
+    //return -100;
+  }
+
+  v >>= 3;
+
+  return v*0.25;
+}
+
+double MAX6675demux::readFahrenheit(int8_t index) {
+  return readCelsius(index) * 9.0/5.0 + 32;
+}
+
+byte MAX6675demux::spiread(void) { 
+  int i;
+  byte d = 0;
+
+  for (i=7; i>=0; i--)
+  {
+    digitalWrite(sclk, LOW);
+    _delay_ms(1);
+    if (digitalRead(miso)) {
+      //set the bit to 0 no matter what
+      d |= (1 << i);
+    }
+
+    digitalWrite(sclk, HIGH);
+    _delay_ms(1);
+  }
+
+  return d;
+}
---
+++ max6675demux.h
@@ -0,0 +1,27 @@
+// this library is public domain. enjoy!
+// www.ladyada.net/learn/sensors/thermocouple
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+class MAX6675demux {
+ public:
+  MAX6675demux(int8_t SCLK, int8_t* DEMUX_PINS, int8_t EN, int8_t MISO, uint8_t ACTIVE_EN=LOW);
+
+  double readCelsius(int8_t index);
+  double readFahrenheit(int8_t index);
+  // For compatibility with older versions:
+  double readFarenheit(int8_t index) { return readFahrenheit(int8_t index); }
+ private:
+  int8_t sclk, miso, en;
+  int8_t* demux_pins;
+  uint8_t active_en, inactive_en;
+  
+  
+  uint8_t spiread(void);
+  
+  void select(int8_t index);
+};
